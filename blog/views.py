298318cc15c46fc4639234b5bf158de362dcd62f
Elmer Bolaños <elmerbolanosgt@gmail.com>
2016-10-14 09:22:18 -0600

    agregue postear y editar articulos.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostearForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
@@ -10,3 +12,29 @@ def post_list(request):
 def detalle_articulo(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/detalle_articulo.html', {'post': post})
+
+def postear_nuevo(request):
+    if request.method == "POST":
+        formulario = PostearForm(request.POST)
+        if formulario.is_valid():
+            post = formulario.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.detalle_articulo', pk=post.pk)
+    else:
+        formulario = PostearForm()
+    return render(request, 'blog/postear_articulo.html', {'formulario': formulario})
+
+def editar_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        formulario = PostearForm(request.POST, instance=post)
+        if formulario.is_valid():
+            post = formulario.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog.views.detalle_articulo', pk=post.pk)
+    else:
+        formulario = PostearForm(instance=post)
+    return render(request, 'blog/postear_articulo.html', {'formulario': formulario})
